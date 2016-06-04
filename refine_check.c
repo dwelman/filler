f5c2d0f76e7eaa8e18b72e0134b98168638819ff
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-06-04 14:26:15 +0200

    ducks

--- refine_check.c
+++ refine_check.c
@@ -6,12 +6,32 @@
 /*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/04 12:42:54 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/06/04 14:00:45 by ddu-toit         ###   ########.fr       */
+/*   Updated: 2016/06/04 14:25:56 by ddu-toit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		is_valid(t_info *info, int x, int y)
 {
+	int	t_x;
+	int	t_y;
+	int	links;
+
+	if (x + info->token.x > info->board.x || y + info->token.y > info->board.y)//size check
+		return (0);
+	while (y < info->token.y)
+	{
+		t_x = 0;
+		while (x < info->token.x)
+		{
+			if (info->token.map[y][x] == info->player
+					&& info->board.map[y][x] == info->player)
+						links++;
+			if (links > 1)
+				return (0);
+			x++;
+		}
+		y++;
+	}
 	//check for overlaps > 1 
 	//check if token placement will fit on map
 }
@@ -31,7 +51,7 @@ void	place_token(t_info *info)
 		y = info->link[i].y - info->token.y + 1;
 		x = info->link[i].x - info->token.x + 1;
 		if (is_valid(info, x, y))
-			info->pos[info->pos_c++] = new_pos(x, y);
+			info->pos[info->pos_c++] = new_pos(x, y, 0);
 		i++;
 	}
 }
