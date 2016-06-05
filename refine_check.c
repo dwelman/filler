1e79263ec6bb65711fd992b4664d0c061fb283ff
Dean DU TOIT <ddu-toit@e5r6p5.jnb.42.fr>
2016-06-05 15:58:02 +0200

    Derp

--- refine_check.c
+++ refine_check.c
@@ -6,7 +6,7 @@
 /*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/04 12:42:54 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/06/05 15:12:54 by ddu-toit         ###   ########.fr       */
+/*   Updated: 2016/06/05 15:54:29 by ddu-toit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 int		is_valid(t_info *info, int x, int y)
 {
+	int	x_o;
 	int	t_x;
 	int	t_y;
 	int	links;
 
-	t_x = 0;
 	t_y = 0;
 	links = 0;
+	x_o = x;
 	if (x + info->token.x > info->board.x || y + info->token.y > info->board.y)//size check
 		return (0);
 	while (t_y < info->token.y)
@@ -29,8 +30,10 @@ int		is_valid(t_info *info, int x, int y)
 		t_x = 0;
 		while (t_x < info->token.x)
 		{
-			if (info->token.map[t_y][t_x] == ft_toupper(info->player)
-					&& info->board.map[y][x] == ft_toupper(info->player))
+			puttrace("trace.txt", &info->token.map[t_y][t_x]);
+			puttrace("trace.txt", &info->board.map[y][x]);
+			if (ft_toupper(info->token.map[t_y][t_x]) == info->player
+					&& ft_toupper(info->board.map[y][x]) == info->player)
 						links++;
 			 puttracen("trace.txt", "links = ", links);
 			if (links > 1)
@@ -38,6 +41,7 @@ int		is_valid(t_info *info, int x, int y)
 			t_x++;
 			x++;
 		}
+		x -= t_x;
 		y++;
 		t_y++;
 	}
@@ -49,6 +53,8 @@ void	place_token(t_info *info)
 	int	i;
 	int	x;
 	int	y;
+	int	tx = 0;
+	int	ty = 0;
 
 	i = 0;
 	info->pos_c = 0;
@@ -58,11 +64,21 @@ void	place_token(t_info *info)
 	{
 		y = info->link[i].y - info->token.y + 1;
 		x = info->link[i].x - info->token.x + 1;
-		if (is_valid(info, x, y))
+		while (ty < info->token.y)
 		{
-			info->pos[info->pos_c++] = new_pos(x, y, 0);
-			puttracen("trace.txt", "x = ", x);
-			puttracen("trace.txt", "y = ", y);
+			while (tx < info->token.x)
+			{
+				if (is_valid(info, x, y))
+				{
+					info->pos[info->pos_c++] = new_pos(x, y, 0);
+					puttracen("trace.txt", "x = ", x);
+					puttracen("trace.txt", "y = ", y);
+				}
+				x++;
+				tx++;
+			}
+			ty++;
+			y++;
 		}
 		i++;
 	}
