ef8fe08ab2353b82e4f0bd1957998e6cda205ddf
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-06-07 08:45:05 +0200

    something

--- refine_check.c
+++ refine_check.c
@@ -6,7 +6,7 @@
 /*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/04 12:42:54 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/06/06 15:45:28 by ddu-toit         ###   ########.fr       */
+/*   Updated: 2016/06/07 08:42:51 by ddu-toit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ int		count_links(t_info *info, int x, int y)
 	int	t_x;
 	int	t_y;
 	int	links;
+	int	star;
 
 	x_org = x;
 	t_y = 0;
 	links = 0;
-	while (t_y < info->token.y)
+	star = 0;
+	while (t_y < info->token.y && star != info->num_str)
 	{
 		t_x = 0;
 		x = x_org;
@@ -30,8 +32,10 @@ int		count_links(t_info *info, int x, int y)
 	//	puttracen("trace.txt", "t_y = ", t_y);
 	//	puttracen("trace.txt", "x = ", x);
 	//	puttracen("trace.txt", "y = ", y, info->fd);
-		while (t_x < info->token.x)
+		while (t_x < info->token.x && star != info->num_str)
 		{
+			if (info->token.map[t_y][t_x] == '*')
+				star++;
 			if (info->token.map[t_y][t_x] == '*'
 					&& ft_toupper(info->board.map[y][x]) == ft_toupper(info->player))
 						links++;
@@ -54,7 +58,7 @@ int		is_valid(t_info *info, int x, int y)
 	int	t_y;
 
 	t_y = 0;
-	if (x + info->token.x > info->board.x || y + info->token.y > info->board.y)//size check
+	if (x + info->token.x > info->board.x || y + info->token.y - 1 > info->board.y)//size check
 		return (0);
 	if (count_links(info, x, y) != 1)
 		return (0);
@@ -72,7 +76,7 @@ int	place_token(t_info *info)
 	i = 0;
 	info->pos_c = 0;
 	info->pos = (t_valid*)malloc(sizeof(t_valid) * info->link_c * info->token.y * info->token.x);
-	while (i < info->link_c) //loop through links found
+	while (i < info->link_c)
 	{
 		y = info->link[i].y - info->token.y + 1;
 		ty = 0;
@@ -86,8 +90,11 @@ int	place_token(t_info *info)
 					if (is_valid(info, x, y))
 					{
 						info->pos[info->pos_c] = new_pos(x, y, 0);
-						//puttracen("trace.txt", "valid x = ", info->pos[info->pos_c].x, info->fd);
-						//puttracen("trace.txt", "valid y = ", info->pos[info->pos_c].y, info->fd);
+						if (y >= 12)
+						{
+							//puttracen("trace.txt", "valid x = ", info->pos[info->pos_c].x, info->fd);
+							//puttracen("trace.txt", "valid y = ", info->pos[info->pos_c].y, info->fd);
+						}
 						info->pos_c++;
 					}
 				x++;
@@ -105,6 +112,16 @@ int	place_token(t_info *info)
 	}
 	else
 	{
+		i = 0;
+		puttrace("","player  : ", info->fd);
+		puttrace("",&info->player, info->fd);
+		puttracen("","link_c = ", info->link_c, info->fd);
+		while (i < info->link_c)
+		{
+			puttracen("","link x = ", info->link[i].x, info->fd);
+			puttracen("","link y = ", info->link[i].y, info->fd);
+			i++;
+		}
 		puttrace("trace.txt", "no valid found", info->fd);
 		ft_putstr("0 0\n");
 		return (0);
